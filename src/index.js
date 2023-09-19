a182467ecd7086d308506ba895598f57faf00382
Nikita Pankov <genozu@yandex.by>
2023-09-19 19:51:50 +0300

    fix: add functionality to a function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    let array = [];
+    let i = 0;
+
+    if (matrix === undefined) {
+        return array;
+    } else {
+        while (i < matrix.length) {
+            if(i % 2 !== 1) {
+            array = array.concat(matrix[i]);
+            } else {
+            array = array.concat(matrix[i].reverse());
+            }
+            i = i + 1;
+        } 
+        return array;
+    }
 }
